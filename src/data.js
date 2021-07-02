138740e90c29bb295551dd70ab7d39c9e10b0542
Gaurav singh <gaurav13021997@gmail.com>
2021-07-02 07:29:00 +0530

    final

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Ankit Anand',
+    age: 21,
+    image:
+      '/ankit.jpeg',
+  },
+  {
+    id: 2,
+    name: 'Avinash Singh',
+    age: 20,
+    image:
+      '/Avinash.jpeg',
+  },
+  {
+    id: 3,
+    name: 'Vipul Pathak',
+    age: 22,
+    image:
+      '/Vipul.jpeg',
+  },
+  {
+    id: 4,
+    name: 'Amit Mathur',
+    age: 22,
+    image:
+      'Mathur.jpeg',
+  },
+  {
+    id: 5,
+    name: 'Vipan Sharma',
+    age: 22,
+    image:
+      'Vipan.jpeg',
+  },
+];
