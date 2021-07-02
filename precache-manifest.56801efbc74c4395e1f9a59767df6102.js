4eb1d4a0e30d6c789ce24a6313c31b8b34c14ecd
Gaurav singh <gaurav13021997@gmail.com>
2021-07-02 07:35:18 +0530

    Updates

---
+++ precache-manifest.56801efbc74c4395e1f9a59767df6102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7865f11ab08f92a2ba3c9b9e5f0d6ab",
+    "url": "/birthday-Card/index.html"
+  },
+  {
+    "revision": "0818c926afe9bced13e0",
+    "url": "/birthday-Card/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-Card/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-Card/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0818c926afe9bced13e0",
+    "url": "/birthday-Card/static/js/main.ea2a6242.chunk.js"
+  },
+  {
+    "revision": "040e21d5e35cec2cd4bf",
+    "url": "/birthday-Card/static/js/runtime-main.893cb863.js"
+  }
+]);
